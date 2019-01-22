7da2efc51d4bb441d713db42b19aa372df23e0aa
Traxan <t-olejniczak@wp.pl>
2019-01-22 18:34:09 +0100

    Project files addition

--- components/App.js
+++ components/App.js
@@ -6,43 +6,34 @@ App = React.createClass({
             gif: {}
         };
     },
-
-    //PROMISE
-
-    getGif: function(searchingText, callback) {
-		var GIPHY_PUB_KEY = 'qYJUoExqhocza5OQXzJpzFgXT2AxuaGW';
-        var GIPHY_API_URL = 'https://api.giphy.com';
-		var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText; 
-		var xhr = new XMLHttpRequest();
-		xhr.open('GET', url);
-
-		xhr.onload = function() {
-			if (xhr.status === 200) {
-				var data = JSON.parse(xhr.responseText).data;
-				var gif = { 
-					url: data.fixed_width_downsampled_url,
-					sourceUrl: data.url
-				};
-			callback(gif);
-			}
-		};
-    xhr.send();
-	},
+    getGif: function (searchingText, callback) {
+        var url = 'https://api.giphy.com' + '/v1/gifs/random?api_key=' + 'Ahz6kKvHoNTrwWXaZYfD2P3uhpLF60NE' + '&tag=' + searchingText;
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url);
+        xhr.onload = function () {
+            if (xhr.status === 200) {
+                var data = JSON.parse(xhr.responseText).data;
+                var gif = {
+                    url: data.fixed_width_downsampled_url,
+                    sourceUrl: data.url
+                };
+                callback(gif);
+            }
+        };
+        xhr.send();
+    },
 
     handleSearch: function (searchingText) {
         this.setState({
             loading: true
         });
-
-        this.getGif(searchingText)
-            .then(gif => {
-                this.setState({
-                    loading: false,
-                    gif: gif,
-                    searchingText: searchingText
-                });
-            })
-            .catch(error => console.log('There is an error: ', error));
+        this.getGif(searchingText, function (gif) {
+            this.setState({
+                loading: false,
+                gif: gif,
+                searchingText: searchingText
+            });
+        }.bind(this));
     },
 
 
@@ -58,7 +49,7 @@ App = React.createClass({
         return ( 
             <div style={styles}>
                 <h1>Wyszukiwarka GIFow!</h1>
-                <p>Znajdź gifa na <a href = 'https://giphy.com'> giphy </a>. Naciskaj enter, aby pobrać kolejne gify.</p>
+                <p>Naciskaj enter, aby pobrać kolejne gify.</p>
                 <Search onSearch = {this.handleSearch}/> 
                 <Gif loading = {this.state.loading} url = {this.state.gif.url} sourceUrl = {this.state.gif.sourceUrl}/>
             </div>
